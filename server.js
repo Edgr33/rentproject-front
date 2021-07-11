197f6045981f04a2159da3d58965695911b23b21
Edgr33 <59263548+Edgr33@users.noreply.github.com>
2021-07-11 05:40:57 -0400

    ed
    configuracion para despliegue de aplicacion en heroku

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist/...'));
+app.use(express.static(__dirname + '/dist/rent'));
 
 app.listen(process.env.PORT || 8000)
 
 //pathLocationStrategy
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist//index.html'));
+  res.sendFile(path.join(__dirname + '/dist/rent/index.html'));
 })
 
 console.log('Console Listening!');
